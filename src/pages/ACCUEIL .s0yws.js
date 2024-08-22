97bf9e2aa35fc99162ab91aa84f15c8a6a7f939c
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-08-22 13:41:36 +0000

    Initial commit

---
+++ src/pages/ACCUEIL .s0yws.js
@@ -0,0 +1,15 @@
+// Référence API Velo : https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+    // Écrivez votre code JavaScript ici en utilisant l'API du framework Velo
+
+    // Affichez « Hello World » :
+    // console.log("Hello world!");
+
+    // Appelez des fonctions sur des éléments de la page. Par exemple :
+    // $w("#button1").label = « Cliquez‑moi ! »;
+
+    // Cliquez sur « Tester » ou accédez à l'aperçu de votre site pour exécuter votre code
+
+});
---
+++ src/pages/PERTE DE POIDS.tnjiu.js
@@ -0,0 +1,10 @@
+ERTE D// API Reference: https://www.wix.com/corvid/reference
+// Code Examples: https://www.wix.com/corvid/examples
+
+$w.onReady(function () {
+	// Write your JavaScript here
+
+	// To select an element by ID use: $w("#elementID")
+
+	// Click "Preview" to run your code
+});
